5065967432d6bc5f18f78a286a54f85504a982e8
jpnykw <jpnykw@gmail.com>
2022-09-18 15:17:02 +0900

    デバッグ用のプロンプトを実装

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 (() => {
-  /* GUI */
+  /* Params */
   const size = 8;
   const scale = 4;
   const px = 24;
+
+  const game = {
+    mode: 'play',
+  };
+
   const mouse = {
     x: 0,
     y: 0,
     selecting: false,
   };
 
-  /* Custom */
+  /* Temporal */
   const apply_stage_template_pattern = (stage) => {
     const length = stage.length;
 
     for (let y = 0; y < 3; y++) {
       stage[y] = stage[y].map((_, x) => {
-        return { color: '#00f', height: (x + y % 2) % 2 };
+        return { color: '#00e', height: (x + y % 2) % 2 };
       })
     }
 
@@ -38,24 +43,28 @@
     });
   }
 
-  const render_cui = (stage) => {
-    const style_props = [];
-    console.log(stage.map((line) =>
-      line.map(({ color, height }) => {
-        style_props.push(`color: ${height === 0 ? 'gray' : color};`);
-        return height > 0 && color ? `%c${height}` : '%c0'
-      }).join(' ')
-    ).join('\n'), ...style_props);
+  const draw_stone_and_level = (level, x, y) => {
+    context.beginPath();
+    context.arc(x + px / 2, y + px / 2, (px / 2) * 0.7, 0, 2 * Math.PI);
+    context.fill();
+
+    // height level
+    context.beginPath();
+    context.textAlign = 'center';
+    context.baseLine = 'bottom';
+    context.font = `${px * 0.6}px Roboto`;
+    context.fillStyle = 'white';
+    context.fillText(level, x + 12, y + px - 7);
   }
 
   const render_gui = (context, stage) => {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     stage.forEach((line, y) => {
-      // console.group();
       line.forEach((cell, x) => {
         const dx = x * px;
         const dy = y * px;
+        const selecting = x === mouse.x && y === mouse.y;
 
         // cell
         context.beginPath();
@@ -63,34 +72,38 @@
         context.rect(dx, dy, px, px);
         context.fill();
 
+        if (game.mode === 'put' && selecting) {
+          context.fillStyle = '#333';
+          draw_stone_and_level(document.querySelector('input').value, dx, dy);
+        }
+
+        if (game.mode === 'remove' && selecting) {
+          context.fillStyle = 'red';
+          draw_stone_and_level('!', dx, dy);
+        }
+
         // stone
         if (cell.height > 0) {
-          context.fillStyle = x === mouse.x && y === mouse.y ? '#c00' : cell.color;
-          context.beginPath();
-          context.arc(dx + px / 2, dy + px / 2, (px / 2) * 0.7, 0, 2 * Math.PI);
-          context.fill();
-
-          // height
-          context.beginPath();
-          context.textAlign = 'center';
-          context.baseLine = 'bottom';
-          context.font = `${px * 0.6}px Roboto`;
-          context.fillStyle = 'white';
-          context.fillText(cell.height, dx + 12, dy + px - 7);
+          if (selecting && game.mode !== 'play') return;
+          context.fillStyle = selecting && game.mode === 'play' ? '#c00' : cell.color;
+          draw_stone_and_level(cell.height, dx, dy);
         }
       });
-      // console.groupEnd();
     });
+
+    if (game.mode !== 'play') {
+      context.fillStyle = 'rgba(255, 255, 255, 0.6)';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
   }
 
   const move = (stage, x, y, dx, dy) => {
     const [nx, ny] = [[x + dx], [y + dy]];
     if (ny < 0 || ny > 7 || nx < 0 || nx > 7) {
-      console.log('範囲外への移動はできません');
+      alert('範囲外への移動はできません');
       return;
     }
 
-    // console.log(x, y, dx, dy, nx, ny);
     const next = stage[ny][nx];
     console.log(next.height);
 
@@ -139,15 +152,20 @@
 
   let game_stage = init_game_stage(size);
   apply_stage_template_pattern(game_stage);
-  // render_cui(game_stage);
   render_gui(context, game_stage);
 
-  // document.querySelector('button').addEventListener('click', () => {
-  //   console.clear();
-  //   move(game_stage);
-  //   // render_cli(game_stage);
-  //   render_gui(context, game_stage);
-  // });
+  /* events */
+  const toggle_put_mode = (target) => {
+    if (game.mode !== 'play' && game.mode !== 'put') return;
+    game.mode = game.mode === 'play' ? 'put' : 'play';
+    target.innerText = game.mode === 'play' ? '設置' : 'キャンセル';
+  }
+
+  const toggle_remove_mode = (target) => {
+    if (game.mode !== 'play' && game.mode !== 'remove') return;
+    game.mode = game.mode === 'play' ? 'remove' : 'play';
+    target.innerText = game.mode === 'play' ? '削除' : 'キャンセル';
+  }
 
   canvas.addEventListener('mousemove', ({ clientX, clientY }) => {
     if (mouse.selecting) return;
@@ -157,7 +175,62 @@
   });
 
   canvas.addEventListener('click', () => {
-    mouse.selecting = !mouse.selecting;
+    if (game.mode === 'play') {
+      mouse.selecting = !mouse.selecting;
+      return;
+    }
+
+    if (game.mode === 'put') {
+      game_stage[mouse.y][mouse.x] = {
+        color: document.querySelector('select').value,
+        height: Number(document.querySelector('input').value),
+      };
+  
+      toggle_put_mode(document.querySelector('#put'));
+      return;
+    }
+
+    if (game.mode === 'remove') {
+      game_stage[mouse.y][mouse.x] = {
+        color: null,
+        height : 0,
+      };
+
+      toggle_remove_mode(document.querySelector('#remove'));
+      return;
+    }
+
+    render_gui(context, game_stage);
+  });
+
+  document.querySelector('#put').addEventListener('click', ({ target }) => {
+    mouse.selecting = false;
+    toggle_put_mode(target);
+    render_gui(context, game_stage);
+  });
+
+  document.querySelector('#remove').addEventListener('click', ({ target }) => {
+    mouse.selecting = false;
+    toggle_remove_mode(target);
+    render_gui(context, game_stage);
+  });
+
+  document.querySelector('#reset').addEventListener('click', () => {
+    game_stage = init_game_stage(size);
+    apply_stage_template_pattern(game_stage);
+    render_gui(context, game_stage);
+  });
+
+  document.querySelector('#clear').addEventListener('click', () => {
+    game_stage = game_stage.map(() => {
+      return new Array(size).fill().map(() => {
+        return {
+          color: null,
+          height: 0,
+        };
+      });
+    });
+    render_gui(context, game_stage);
   });
 
   document.addEventListener('keydown', ({ key }) => {
